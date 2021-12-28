2d06c487a62a03405058bb1c6de7e2926320aa9b
AScully24 <anthony.scully24@gmail.com>
2021-12-28 11:10:57 +0000

    Entry config added
    Signed-off-by: AScully24 <anthony.scully24@gmail.com>

---
+++ src/main/kotlin/com/laces/form/FormEntryConfiguration.kt
@@ -0,0 +1,8 @@
+package com.laces.form
+
+import org.springframework.boot.SpringBootConfiguration
+import org.springframework.context.annotation.ComponentScan
+
+@ComponentScan
+@SpringBootConfiguration
+class FormEntryConfiguration
